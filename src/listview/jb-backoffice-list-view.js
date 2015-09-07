a0df4f2e6db6d960234ae0316fab9ee560c3cb9c
Felix Steiner <felix_steiner@bluewin.ch>
2015-09-07 19:29:50 +0200

    API breaking change: Switched from ng-route to ui-router

--- src/listview/jb-backoffice-list-view.js
+++ src/listview/jb-backoffice-list-view.js
@@ -1,90 +1,93 @@
-'use strict';
+( function() {
+
+	'use strict';
 
 
-/**
-* Renders all default stuff for a listview, including
-* - title
-* - notifications
-* - add button
-* Transcludes datatable for the entity.
-*/
-angular
-.module( 'jb.backofficeListView', [] )
-.directive( 'listView', [ '$location', '$filter', function( $location, $filter ) {
+	/**
+	* Renders all default stuff for a listview, including
+	* - title
+	* - notifications
+	* - add button
+	* Transcludes datatable for the entity.
+	*/
+	angular
+	.module( 'jb.backofficeListView', [] )
+	.directive( 'listView', [ '$filter', '$state', '$stateParams', function( $filter, $state, $stateParams ) {
 
-	return {
-		link			: function( scope, element, attrs ) {
+		return {
+			link			: function( scope, element, attrs ) {
+
+				var entityName = $stateParams.entityName
+					, entityId = $stateParams.entityId;
+
+				// TRANSLATIONS
+				// Set translations
+				scope.titleText = $filter( 'translate' )( 'web.backoffice.' + entityName + '.maintitle' );
+				scope.addText = $filter( 'translate' )( 'web.backoffice.' + entityName + '.add' );
+
+				// FUNCTIONS
+				// Redirect functions for add and edit
+				scope.add = function() {
+					$state.go( 'app.detail', { entityName: entityName, entityId: '/new' } );
+				};
+
+				scope.edit = function( id ) {
+					$state.go( 'app.detail', { entityName: entityName, entityId: id } );
+				};
 
 
-			// Path (e.g. artist); remove leading slash
-			var entity = $location.path().substring( 1 );
+				// Edit fields
+				// - Prepend with ID
+				// - Append edit button
+				if( scope.fields ) {
+					// ID
+					scope.fields.unshift( {
+						title		: 'ID'
+						, selector	: 'id'
+					} );
+					// Edit
+					scope.fields.push( {
+						content: function( data ) {
+							return '<button class="btn btn-default btn-sm" data-ng-click="edit('+data.id+')">' + $filter( 'translate' )( 'web.backoffice.edit' ) + '</button>';
+						}
+					} );
+				}
 
-			// TRANSLATIONS
-			// Set translations
-			scope.titleText = $filter( 'translate' )( 'web.backoffice.' + entity + '.maintitle' );
-			scope.addText = $filter( 'translate' )( 'web.backoffice.' + entity + '.add' );
 
-			// FUNCTIONS
-			// Redirect functions for add and edit
-			scope.add = function() {
-				$location.path( '/' + entity + '/new' );
-			};
-
-			scope.edit = function( id ) {
-				$location.path( '/' + entity + '/' + id );
-			};
-
-
-			// Edit fields ( was initiated in ebBackoffice[EntityName])
-			// - Prepend with ID
-			// - Append edit button
-			if( scope.fields ) {
-				// ID
-				scope.fields.unshift( {
-					title		: 'ID'
-					, selector	: 'id'
-				} );
-				// Edit
-				scope.fields.push( {
-					content: function( data ) {
-						return '<button class="btn btn-default btn-sm" data-ng-click="edit('+data.id+')">' + $filter( 'translate' )( 'web.backoffice.edit' ) + '</button>';
-					}
-				} );
 			}
+			, templateUrl	: 'listViewTemplate.html'
+			, transclude	: true
+		};
 
+	} ] )
+	.run( function( $templateCache ) {
 
-		}
-		, templateUrl	: 'listViewTemplate.html'
-		, transclude	: true
-	};
+		$templateCache.put( 'listViewTemplate.html',
+			'<div clas=\'col-xs-12\'>' +
+				'<h3>{{ titleText }}</h3>' +
+				'<div class=\'boxed\'>' +
+					'<div class=\'inner\'>' +
 
-} ] )
-.run( function( $templateCache ) {
-
-	$templateCache.put( 'listViewTemplate.html',
-		'<div clas=\'col-xs-12\'>' +
-			'<h3>{{ titleText }}</h3>' +
-			'<div class=\'boxed\'>' +
-				'<div class=\'inner\'>' +
-
-					'<div notification></div>' +
-					'<div class=\'row list-menu\'>' +
-						'<div class=\'col-xs-12\'>' +
-							'<button type=\'button\' class=\'btn btn-sm btn-primary pull-right\' ng-click=\'add()\'>' +
-								'<i class=\'fa fa-plus\'></i>' +
-								'{{ addText }}' +
-							'</button>' +
+						'<div notification></div>' +
+						'<div class=\'row list-menu\'>' +
+							'<div class=\'col-xs-12\'>' +
+								'<button type=\'button\' class=\'btn btn-sm btn-primary pull-right\' ng-click=\'add()\'>' +
+									'<i class=\'fa fa-plus\'></i>' +
+									'{{ addText }}' +
+								'</button>' +
+							'</div>' +
 						'</div>' +
-					'</div>' +
-					'<div class=\'row\'>' +
-						'<div class=\'col-xs-12\'>' +
-							// Datatable goes here
-							'<div data-ng-transclude></div>' +
+						'<div class=\'row\'>' +
+							'<div class=\'col-xs-12\'>' +
+								// Datatable goes here
+								'<div data-ng-transclude></div>' +
+							'</div>' +
 						'</div>' +
 					'</div>' +
 				'</div>' +
-			'</div>' +
-		'</div>'
-	);
+			'</div>'
+		);
 
-} );
+	} );
+
+} )();
